88daff3e4eaadf58adcb80fe4c5ef378c4390473
Sumer7121 <143977296+Sumer7121@users.noreply.github.com>
2024-10-19 20:02:00 +0000

    image fix and box fix

--- my-portfolio/src/scenes/Landing.jsx
+++ my-portfolio/src/scenes/Landing.jsx
@@ -14,20 +14,20 @@ const Landing = ({ setSelectedPage }) => {
       <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
         {isAboveLarge ? (
           <div
-            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
-            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-5 before:rounded-t-[400px]
+            before:w-full before:max-w-[200px] md:before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]"
           >
             <img
               alt="profile"
               className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-              src="assets/profile-image.png"
+              src="assets/prof-image.png"
             />
           </div>
         ) : (
           <img
             alt="profile"
             className="z-10 w-full max-w-[400px] md:max-w-[600px]"
-            src="assets/profile-image.png"
+            src="assets/prof-image.png"
           />
         )}
       </div>
